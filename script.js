17555d2d8914d2810bbdcbdf80d23c7c447cdcf5
mlaane <goobric@gmail.com>
2023-12-23 15:27:00 +0000

    added variables for calCount budgetNI entryDD

---
+++ script.js
@@ -0,0 +1,7 @@
+// JS Code for Calorie Counter
+
+const calorieCounter = document.getElementById('calorie-counter');
+
+const budgetNumberInput = document.getElementById('budget');
+
+const entryDropdown = document.getElementById('entry-dropdown');
