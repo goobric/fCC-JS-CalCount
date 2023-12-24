8f5ea6a7bdf2faea692b77767c7f135de7907676
mlaane <goobric@gmail.com>
2023-12-24 20:44:58 +0000

    updated

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 
 const output = document.getElementById('output');
+
+/**  The split() method splits a string into an array of substrings, and returns the new array. You can pass in an optional separator which tells the method where each split should happen.
+For example, passing an empty string into the split method will split the string into an array of individual characters.
+const str = 'Hello World';
+const strArray = str.split('');
+// ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
+**/
+function cleanInputString(str) {
+  const strArray = str.split('');
+  const cleanStrArray = [];
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i] !== ' ') {
+      cleanStrArray.push(strArray[i]);
+    }
+  }
+}
