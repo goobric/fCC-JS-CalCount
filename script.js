81690da9ca0f94413a0de89fe87b9b32aa454571
mlaane <goobric@gmail.com>
2023-12-26 00:03:10 +0000

    add calcCal func

--- script.js
+++ script.js
@@ -87,6 +87,35 @@ The querySelectorAll() method returns a NodeList of all the elements that match 
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault(); // Call preventDefault() on the event object
+  isError = false; // Reset the global error flag to false
+}
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+  const breakfastNumberInputs = document.querySelectorAll(
+    '#breakfast input[type=number]'
+  );
+  const lunchNumberInputs = document.querySelectorAll(
+    '#lunch input[type=number]'
+  );
+  const dinnerNumberInputs = document.querySelectorAll(
+    '#dinner input[type=number]'
+  );
+  const snacksNumberInputs = document.querySelectorAll(
+    '#snacks input[type=number]'
+  );
+  const exerciseNumberInputs = document.querySelectorAll(
+    '#exercise input[type=number]'
+  );
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
